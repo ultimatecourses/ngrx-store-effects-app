d9a0b8fa95216a7788b2d540608dc24a19692677
Todd Motto <todd@toddmotto.com>
2017-11-26 17:08:09 +0000

    Step 1: First Actions

---
+++ src/products/store/actions/pizzas.action.ts
@@ -0,0 +1,25 @@
+import { Action } from '@ngrx/store';
+
+import { Pizza } from '../../models/pizza.model';
+
+// load pizzas
+export const LOAD_PIZZAS = '[Products] Load Pizzas';
+export const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail';
+export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
+
+export class LoadPizzas implements Action {
+  readonly type = LOAD_PIZZAS;
+}
+
+export class LoadPizzasFail implements Action {
+  readonly type = LOAD_PIZZAS_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class LoadPizzasSuccess implements Action {
+  readonly type = LOAD_PIZZAS_SUCCESS;
+  constructor(public payload: Pizza[]) {}
+}
+
+// action types
+export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
