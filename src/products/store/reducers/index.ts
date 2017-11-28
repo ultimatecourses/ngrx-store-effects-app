52d2a06771ad54ae035115551f8795da5bc66a13
Todd Motto <todd@toddmotto.com>
2017-11-28 16:19:52 +0000

    Step 10: Toppings Reducer

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,13 +1,16 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
+import * as fromToppings from './toppings.reducer';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer,
 };
 
 export const getProductsState = createFeatureSelector<ProductsState>(
---
+++ src/products/store/reducers/toppings.reducer.ts
@@ -0,0 +1,65 @@
+import * as fromToppings from '../actions/toppings.action';
+import { Topping } from '../../models/topping.model';
+
+export interface ToppingsState {
+  entities: { [id: number]: Topping };
+  loaded: boolean;
+  loading: boolean;
+}
+
+export const initialState: ToppingsState = {
+  entities: {},
+  loaded: false,
+  loading: false,
+};
+
+export function reducer(
+  state = initialState,
+  action: fromToppings.ToppingsAction
+): ToppingsState {
+  switch (action.type) {
+    case fromToppings.LOAD_TOPPINGS: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
+    case fromToppings.LOAD_TOPPINGS_SUCCESS: {
+      const toppings = action.payload;
+
+      const entities = toppings.reduce(
+        (entities: { [id: number]: Topping }, topping: Topping) => {
+          return {
+            ...entities,
+            [topping.id]: topping,
+          };
+        },
+        {
+          ...state.entities,
+        }
+      );
+
+      return {
+        ...state,
+        loaded: true,
+        loading: false,
+        entities,
+      };
+    }
+
+    case fromToppings.LOAD_TOPPINGS_FAIL: {
+      return {
+        ...state,
+        loaded: false,
+        loading: false,
+      };
+    }
+  }
+
+  return state;
+}
+
+export const getToppingEntities = (state: ToppingsState) => state.entities;
+export const getToppingsLoaded = (state: ToppingsState) => state.loaded;
+export const getToppingsLoading = (state: ToppingsState) => state.loading;
