c6feb70fc4edec613577000d93a41e508d918ba7
Alberto Aldegheri <aaldegheri@terranovasoftware.eu>
2017-12-08 12:59:22 +0100

    New NgRx Helper

---
+++ ubik_modules/redux/index.ts
@@ -0,0 +1,113 @@
+import { Action } from '@ngrx/store';
+import { Type } from '@angular/core';
+
+export interface ActionWithPayload<T> extends Action {
+  payload: T;
+}
+
+export interface StateActionBase<TState> {
+  type: string;
+  reduce?: (state: TState, payload?: any) => TState;
+}
+
+export class StateActionVoid<TState> implements StateActionBase<TState> {
+  constructor(
+    public readonly type: string
+  ) {}
+}
+
+export class StateActionPayload<TState, TPayload> implements StateActionBase<TState> {
+  constructor(
+    public readonly type: string,
+    public readonly reduce: (state: TState, payload: TPayload) => TState
+  ) {
+    // TODO Dynamic Register in singleton
+  }
+  readonly action = function(payload: TPayload): Action | ActionWithPayload<TPayload> {
+    return {
+      type: this.type,
+      payload
+    };
+  }
+}
+
+export class StateAction<TState> extends StateActionPayload<TState, void> {
+  constructor(
+    public readonly type: string,
+    public readonly reduce: (state: TState) => TState
+  ) {
+    super(type, reduce);
+  }
+
+  readonly action = function(): Action {
+    return {
+      type: this.type
+    };
+  }
+}
+
+export class StateHandler<TState> {
+  private readonly actionMap: { [actionType: string] : StateActionBase<TState> } = {};
+  constructor(
+    public stateId: string,
+    private initialState: TState
+  ) { }
+
+  reducer(): (state: TState, action: Action | ActionWithPayload<any>) => TState {
+    const actionMap = this.actionMap;
+    const initialState = this.initialState;
+    return function reducer(state: TState = initialState, action: ActionWithPayload<any>): TState {
+      const actionReducer = actionMap[action.type];
+      return actionReducer && actionReducer.reduce ? actionReducer.reduce(state, action.payload) : state;
+    }
+  }
+
+  action(
+    type: string,
+    reduce?: (state: TState) => TState
+  ): StateAction<TState> {
+    const action = new StateAction(type, reduce);
+    // register action only if reducer has been specified
+    if (reduce) {
+      this.actionMap[type] = action;
+    }
+    return action;
+  }
+
+  actionWithPayload<TPayload>(
+    type: string,
+    reduce: (state: TState, payload: TPayload) => TState
+  ): StateActionPayload<TState, TPayload> {
+    const action = new StateActionPayload(type, reduce);
+    this.actionMap[type] = action;
+    return action;
+  }
+}
+
+// TEST USAGE
+
+// export interface FooState {
+//   index: number;
+// }
+
+// export const FooHandler = new StateHandler<FooState>('foo', { index: 0 });
+
+// export const increment = FooHandler.action(
+//   '[Foo] Increment',
+//   function(s: FooState) {
+//     return {
+//       ...s,
+//       index: s.index + 1
+//     };
+//   }
+// );
+
+// export const incrementBy = FooHandler.actionWithPayload<number>(
+//   '[Foo] Increment',
+//   function(s, by) {
+//     return {
+//       ...s,
+//       index: s.index + by
+//     };
+//   }
+// );
