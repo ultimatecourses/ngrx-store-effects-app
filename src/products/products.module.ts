048ee945966351cd6ee70ca147af0a1124c886f4
Petro Deriy <petroderiy1@gmail.com>
2017-12-26 11:26:51 +0100

    Creating and Registering our Reducer

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+
+import { reducers } from './store';
+
 // components
 import * as fromComponents from './components';
 
@@ -35,6 +39,7 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
+    StoreModule.forFeature('products', reducers)
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
