856a117b1c8ae76dcc30713f2c025a08d17554ab
Petro Deriy <petroderiy1@gmail.com>
2018-01-02 12:09:42 +0100

    Multiple Reducers

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,12 +1,15 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducers';
+import * as fromToppings from './toppings.reducer';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer
 };
 
 export const getProductsState = createFeatureSelector<ProductsState>(
