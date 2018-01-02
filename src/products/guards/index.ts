1a6937a80af0af4f1bf3449b6b3cdb575929d8db
Petro Deriy <petroderiy1@gmail.com>
2018-01-02 21:37:48 +0100

    Preloading across multiple Routes

---
+++ src/products/guards/index.ts
@@ -0,0 +1,5 @@
+import { PizzasGuard } from './pizzas.guard';
+
+export const guards: any[] = [PizzasGuard];
+
+export * from './pizzas.guard';
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -11,6 +11,8 @@ import { effects, reducers } from './store';
 import * as fromComponents from './components';
 // containers
 import * as fromContainers from './containers';
+// guards
+import * as fromGuards from './guards';
 // services
 import * as fromServices from './services';
 
@@ -18,10 +20,12 @@ import * as fromServices from './services';
 export const ROUTES: Routes = [
   {
     path: '',
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductsComponent,
   },
   {
     path: 'new',
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductItemComponent,
   },
   {
@@ -39,7 +43,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature('products', reducers),
     EffectsModule.forFeature(effects)
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards ],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
